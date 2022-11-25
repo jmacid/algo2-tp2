50d5b8c44aebe5645e58a9d668668b332cc43646
Juan Martin Nazarian <jmnazarian@gmail.com>
2022-11-25 00:36:00 -0300

    Change cell genetic values

--- src/tests/testCelula.cpp
+++ src/tests/testCelula.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "../tda/Celula.h"
 
@@ -10,17 +11,26 @@ int main() {
 
     cout << "Estado celula: " << cell1->getEstado() << endl;
 
+    cell1->actualizarGen(1, 25);
+    cell1->actualizarGen(2, 125);
+    cell1->actualizarGen(3, 250);
+
     unsigned int longuitud = cell1->getCantidadGenes();
     for(int i = 0; i < longuitud; i++){
       cout << cell1->getCargaGenetica(i+1) << endl;
     }
+
 
     delete cell1;
     
   }
   catch(char const* excp){
     cout << "Excepcion: " << excp << endl;
-  } catch(...){
+  }
+  catch(string message){
+    cout << "Excepcion: " << message << endl;
+  }
+  catch(...){
     cout << "Excepcion desconocida" << endl;
   }
   return 0;
